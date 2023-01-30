f418c56a4af736804c110744d924c72cd43641c1
sagegod <cdeng1999@gmail.com>
2023-01-30 12:12:19 -0800

    added npm packages

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 //Import and require mysql2
 const mysql = require('mysql2')
-//Import and require inquirer
-const inquirer = require("inquirer")
-//Import and require asciiart
-const logo = require('asciiart-logo');
 
+//Impoirt and require express
+const express = require('express');
+
+const path = require('path');
+const helpers = require('./utils/helpers');
+
+const app = express();
+
+require(`dotenv`).config();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 // Connect to database
 const db = mysql.createConnection(
     {
